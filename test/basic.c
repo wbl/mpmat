ba7995c549ec1278b057c1f537ce17417c228b28
Watson Ladd <watsonbladd@gmail.com>
2014-03-23 11:54:06 -0400

    Fixed problems, changed compiler, and odd makefile issue

--- test/basic.c
+++ test/basic.c
@@ -2,31 +2,32 @@
 #include <stdlib.h>
 #include<mpc.h>
 #include "mpmat.h"
-void main(){
+int main(){
   mpmat A = mpmat_init(2,2);
   mpmat B = mpmat_init(2,2);
   mpmat C = mpmat_init(2,2);
   mpc_t tmp;
   mpc_rnd_t rnd=MPC_RNDNN;
   mpc_init2(tmp, 256);
-  mpc_set_d(tmp, 1.0d, rnd);
+  mpc_set_d(tmp, 1.0, rnd);
   mpmat_set(A,1,1,tmp);
-  mpc_set_d(tmp, 2.0d, rnd);
+  mpc_set_d(tmp, 2.0, rnd);
   mpmat_set(A,1,2, tmp);
-  mpc_set_d(tmp, 3.0d, rnd);
+  mpc_set_d(tmp, 3.0, rnd);
   mpmat_set(A, 2, 1, tmp);
-  mpc_set_d(tmp, 4.0d, rnd);
+  mpc_set_d(tmp, 4.0, rnd);
   mpmat_set(A,2,2,tmp);
   
-  mpc_set_d(tmp, 1.0d, rnd);
+  mpc_set_d(tmp, 1.0, rnd);
   mpmat_set(B,1,1,tmp);
-  mpc_set_d(tmp, 0.0d, rnd);
+  mpc_set_d(tmp, 0.0, rnd);
   mpmat_set(B,1,2, tmp);
-  mpc_set_d(tmp, 1.0d, rnd);
+  mpc_set_d(tmp, 0.0, rnd);
   mpmat_set(B, 2, 1, tmp);
-  mpc_set_d(tmp, 0.0d, rnd);
+  mpc_set_d(tmp, 1.0, rnd);
   mpmat_set(B,2,2,tmp);
 
+  mpmat_mul(C, A,B);
   for(int i=1; i<=2; i++){
     for(int j=1; j<=2; j++){
       fprintf(stdout, "C[%d,%d]=", i, j);
