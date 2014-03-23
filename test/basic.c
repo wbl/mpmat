d1de62a99e1c5a33a002aa22cc4f7b8c21124f92
Watson Ladd <watsonbladd@gmail.com>
2014-03-23 11:35:42 -0400

    Tests don't work

---
+++ test/basic.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<mpc.h>
+#include "mpmat.h"
+void main(){
+  mpmat A = mpmat_init(2,2);
+  mpmat B = mpmat_init(2,2);
+  mpmat C = mpmat_init(2,2);
+  mpc_t tmp;
+  mpc_rnd_t rnd=MPC_RNDNN;
+  mpc_init2(tmp, 256);
+  mpc_set_d(tmp, 1.0d, rnd);
+  mpmat_set(A,1,1,tmp);
+  mpc_set_d(tmp, 2.0d, rnd);
+  mpmat_set(A,1,2, tmp);
+  mpc_set_d(tmp, 3.0d, rnd);
+  mpmat_set(A, 2, 1, tmp);
+  mpc_set_d(tmp, 4.0d, rnd);
+  mpmat_set(A,2,2,tmp);
+  
+  mpc_set_d(tmp, 1.0d, rnd);
+  mpmat_set(B,1,1,tmp);
+  mpc_set_d(tmp, 0.0d, rnd);
+  mpmat_set(B,1,2, tmp);
+  mpc_set_d(tmp, 1.0d, rnd);
+  mpmat_set(B, 2, 1, tmp);
+  mpc_set_d(tmp, 0.0d, rnd);
+  mpmat_set(B,2,2,tmp);
+
+  for(int i=1; i<=2; i++){
+    for(int j=1; j<=2; j++){
+      fprintf(stdout, "C[%d,%d]=", i, j);
+      mpmat_get(tmp, C, i, j);
+      mpc_out_str(stdout, 10, 10,tmp,  rnd);
+      fprintf(stdout, "\n");
+    }
+  }
+
+  exit(0);
+}
